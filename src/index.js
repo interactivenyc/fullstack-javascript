f371c23e4f3e818a5119d2f1491b26107cfed0a9
Steve Warren <steve@interactivenyc.com>
2018-04-07 15:22:37 -0400

    adding prop-types module to replace React.PropTypes which has been deprecated

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const App = (props) => {
+  return(
+    <h2 className="text-center">
+      {props.headerMessage}
+    </h2>
+  );
+};
+
+App.propTypes = {
+  headerMessage: PropTypes.string.isRequired
+};
 
 ReactDOM.render(
-  <h2 className="text-center">
-    Hello React with JSX!!
-  </h2>,
+  <App headerMessage="Hello Props"/>,
   document.getElementById('root')
 );
